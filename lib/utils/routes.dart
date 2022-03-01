a85d32cb4cf774e0b53d2723874169e7b17df249
developmentmrinc12 <development@mrintelligenceinc.com>
2022-03-01 19:59:07 +0530

    update

---
+++ lib/utils/routes.dart
@@ -0,0 +1,6 @@
+import 'package:explore_academy_superadmin/auth_page/splash_screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => const SplashScreen(),
+};
